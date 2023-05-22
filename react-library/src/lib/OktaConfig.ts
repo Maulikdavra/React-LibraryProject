eeaa103fb54831ab06793173a10951801504d943
Maulik Davra <maulikdavra06@gmail.com>
2023-05-21 23:03:44 -0500

    add

--- react-library/src/lib/OktaConfig.ts
+++ react-library/src/lib/OktaConfig.ts
@@ -1,9 +1,8 @@
 export const OktaConfig = {
     clientId: '0oa877vzlhjL5qRG45d7',
     issuer: 'https://dev-89466154.okta.com/oauth2/default',  // Dev environment that links to an HTTPS secure line to our off default or redirect URI.
-    redirectUri: 'https://localhost:3000/login/callback',
+    redirectUri: 'https://maulikdavra.github.io/login/callback',
     scope: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpCheck: true,
-    
-}       
+}           
